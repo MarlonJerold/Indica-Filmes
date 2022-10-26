0436f687ef2a361c5ee9faaa8038567c877c7eb8
MarlonJerold <jeroldmarlon12@gmail.com>
2022-10-25 23:36:00 -0300

    Criação de função que conecta e extrai arquivos Json em formato de tabela

--- enviomsg.py
+++ enviomsg.py
@@ -1,11 +1,14 @@
 import smtplib
 import email.message
 import requests
+import pandas as pd
+import json
 
-requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
-requisicao_dicionario = requisicao.json()
-cotacao = float(requisicao_dicionario['USDBRL']['bid'])
-print(cotacao)
+def api_dolar():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
+    requisicao_dicionario = requisicao.json()
+    cotacao = float(requisicao_dicionario['USDBRL']['bid'])
+    print(cotacao)
 
 def envioemail():   
     corpo_email = f"""
@@ -15,7 +18,7 @@ def envioemail():
     msg['Subject'] = "Python"
     msg['From'] = 'jeroldmarlon12@gmail.com'
     msg['To'] = 'jeroldmarlon12@gmail.com'
-    password = '****'
+    password = '***'
     msg.add_header('Content-Type', 'text/html')
     msg.set_payload(corpo_email )
     
@@ -25,5 +28,18 @@ def envioemail():
     s.sendmail(msg['From'], [msg['To']], msg.as_string().encode('utf-8'))
     print('Email enviado')
 
-if(cotacao < 6):
-    envioemail()
+def conect_api_filmes():
+    conect = requests.get("https://imdb-api.com/en/API/Top250Movies/k_l89y0mfu")
+    valor_requisicao = conect.json()
+    print(valor_requisicao)
+    
+    value = 0
+    while value < 200:
+        keys = valor_requisicao['items'][value].keys()  
+        values = valor_requisicao['items'][value].values()
+        value = value + 1 
+        df = pd.DataFrame(values, index=keys, columns=[''])
+        print("________________")
+        print(df)
+
+conect_api_filmes()    

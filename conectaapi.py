c6214087a63b3d5bf669425a678bd160b9eee8f0
MarlonJerold <jeroldmarlon12@gmail.com>
2022-10-28 01:06:27 -0300

    Criação do método que gera filmes aletório juntamente com o método que envia email a pesso

--- conectaapi.py
+++ conectaapi.py
@@ -1,16 +1,10 @@
-import smtplib
-import email.message
 import requests
-import pandas as pd
-from random import randint 
-
 
 class Conexao:
     
-    def leituraJson():
+    def top200filmesAPI():
         conect = requests.get("https://imdb-api.com/en/API/Top250Movies/k_l89y0mfu")
         valor_requisicao = conect.json()
         return valor_requisicao
-    
 
 

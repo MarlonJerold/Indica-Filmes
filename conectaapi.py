673bbfb456f6b01bd686577ed2e1e269df2c1359
MarlonJerold <jeroldmarlon12@gmail.com>
2022-10-27 23:46:17 -0300

    Estudo de manipulação de Json

--- conectaapi.py
+++ conectaapi.py
@@ -1,16 +1,35 @@
 import smtplib
 import email.message
+
 import requests
-import pandas as pd
+import pandas as rs
 from random import randint 
+import json
 
 
 class Conexao:
     
-    def leituraJson():
-        conect = requests.get("https://imdb-api.com/en/API/Top250Movies/k_l89y0mfu")
-        valor_requisicao = conect.json()
-        return valor_requisicao
+    
+    conect = requests.get("https://imdb-api.com/en/API/Top250Movies/k_l89y0mfu")
+    obj = conect.json()
+    
+    posicao = 0
+    while posicao < 2:
+        keys = obj['items'][posicao].keys()
+        values = obj['items'][posicao].values()
+        teste = obj['items'][posicao]['title']
+        
+        
+        print(teste)
+        
+        
+        valor = rs.DataFrame(values, index=keys)
+        print(valor)
+        posicao += 1
+    
+
+
+    
     
 
 
--- filmes.py
+++ filmes.py
@@ -15,15 +15,12 @@ class Filmes:
     apiJson = conectar.leituraJson()
                 
     def Top200filmes(apiJson, number):                         
-        keys = apiJson['imDbId'][number].keys()  
-        values = apiJson['imDbId'][number].values()
+        keys = apiJson['items'][number].keys()  
+        values = apiJson['items'][number].values()
         dadostratado = pd.DataFrame(values, index=keys)
-        print(dadostratado)
+        return dadostratado
         
-    def filme200aleatorio(apiJson, number):
-        number = range(1,200)    
-        keys = apiJson['imDbId'][number].keys()  
-        values = apiJson['imDbId'][number].values()
-        dadostratado = pd.DataFrame(values, index=keys)
-        print(dadostratado)   
-        
+
+          
+    
+    
